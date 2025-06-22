4cba21d96ec21e6a180323fe1329d82561718509
Sonu <Sonugpc@gmail.com>
2025-06-22 23:58:54 +0530

    Fixed

--- src/ChannelPostUpdatesHandler.js
+++ src/ChannelPostUpdatesHandler.js
@@ -2,13 +2,14 @@ const Config = require("../config");
 const { BufferApi } = require("./BufferApi");
 const CaptionModifier = require("./CaptionModifier");
 const handleChannelPostHandler = async (ctx) => {
+  
   const buffer = new BufferApi(Config.BUFFER_API);
 
   if (ctx.channelPost.text) {
-    const text = await CaptionModifier.modifyCaption(ctx.channelPost.text);
+    const text =ctx.channelPost.text;
     await buffer.postToBuffer(Config.Profiles, text, true, null);
   } else if (ctx.channelPost.photo) {
-    const text = await CaptionModifier.modifyCaption(ctx.channelPost.caption);
+    const text =ctx.channelPost.caption
 
     const media = await ctx.telegram.getFileLink(
       ctx.channelPost.photo[2].file_id
@@ -25,5 +26,7 @@ const handleDirectPostHandler = async (ctx) => {
     await buffer.postToBuffer(Config.Profiles, text, true, null);
   }
 };
+
+const blackListWords = ['loot', 'Loot'];
 
 module.exports = { handleChannelPostHandler, handleDirectPostHandler };
